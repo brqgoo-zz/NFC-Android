5dac055fc50943141ac2641f0075af14b8f716f4
brqgoo <64094715+brqgoo@users.noreply.github.com>
2020-10-15 11:40:41 +0300

    init

---
+++ app/src/main/java/com/example/nfc_android/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nfc_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        System.out.println("selam");
+
+
+    }
+}
